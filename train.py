d24a8586a2725e9487965cb46964103d45e43ee9
Olivier Benson-Smith <Ollie@Olivias-MacBook-Pro-2.local>
2022-03-09 13:17:00 +0000

    model.py added with NeuralNet functionality

--- train.py
+++ train.py
@@ -4,8 +4,9 @@ import numpy as np
 from nltk_utils import tokenize, stem, bag_of_words
 import torch
 import torch.nn as nn
-import torch.utils.data 
-import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader
+
+from model import NeuralNet
 
 with open('intents.json', 'r') as f:
     intents = json.load(f)
@@ -26,3 +27,41 @@ ignore_words = ['?', '!', '.', ',']
 all_words = [stem(w) for w in all_words if w not in ignore_words]
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
+
+
+x_train = []
+y_train = []
+
+for (pattern_sentence, tag) in xy:
+    bag = bag_of_words(pattern_sentence, all_words)
+    x_train.append(bag)
+
+    label = tags.index(tag)
+    y_train.append(label) #CrossEntropyLoss
+
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+
+class ChatDataSet(Dataset):
+    def __init__(self):
+        self.n_samples = len(x_train)
+        self.x_data = x_train
+        self.y_data = y_train
+
+    #dataset[idx]
+    def __getitem__(self, index):
+        return self.x_data[idx], self.y_data[idx]
+
+    def __len__(self):
+        return self.n_samples
+
+#Hyperparameters
+batch_size = 8 
+hidden_size = 8
+output_size = len(tags)
+input_size = len(x_train[0])
+
+dataset = ChatDataSet()
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+model = NeuralNet(input_size, hidden_size, output_size)
